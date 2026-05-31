9c20cae8722f85c88f51767108e884ad045044b2
DevanshJoshi2006 <devansh2420@gmail.com>
2026-05-31 05:52:24 +0530

    Enhance AverageCalculator to support custom meals
    Added handling for custom meals in getAvg method.

--- FOOD_WASTE_MANAGEMENT_SYSTEM/AverageCalculator.java
+++ FOOD_WASTE_MANAGEMENT_SYSTEM/AverageCalculator.java
@@ -1,39 +1,67 @@
 public class AverageCalculator {
 
-    public static double[] getAvg(String meal, int people) {
+    public static double[] getAvg(
+            String meal,
+            int people
+    ) {
 
-        double roti = 0, dal = 0, sabji = 0, rice = 0;
+        double roti = 0,
+               dal = 0,
+               sabji = 0,
+               rice = 0;
 
-        // Roti meals
-        if (meal.equals("Dal+Roti")) {
+        // ROTI MEALS
+        if(meal.equals("Dal+Roti")) {
+
             roti = 2 * people;
             dal = 1 * people;
-        } 
-        else if (meal.equals("Sabji+Roti")) {
+        }
+
+        else if(meal.equals("Sabji+Roti")) {
+
             roti = 2 * people;
             sabji = 1 * people;
-        } 
-        else if (meal.equals("Dal+Roti+Sabji")) {
+        }
+
+        else if(meal.equals("Dal+Roti+Sabji")) {
+
             roti = 2 * people;
             dal = 0.5 * people;
             sabji = 0.5 * people;
         }
 
-        // Rice meals
-        else if (meal.equals("Rice+Dal")) {
+        // RICE MEALS
+        else if(meal.equals("Rice+Dal")) {
+
             rice = 1 * people;
             dal = 1 * people;
-        } 
-        else if (meal.equals("Rice+Sabji")) {
+        }
+
+        else if(meal.equals("Rice+Sabji")) {
+
             rice = 1 * people;
             sabji = 1 * people;
-        } 
-        else if (meal.equals("Rice+Dal+Sabji")) {
+        }
+
+        else if(meal.equals("Rice+Dal+Sabji")) {
+
             rice = 1 * people;
             dal = 0.5 * people;
             sabji = 0.5 * people;
         }
 
-        return new double[]{roti, dal, sabji, rice};
+        // CUSTOM FOOD
+        else {
+
+            return CustomMealManager
+                    .getDefault(people);
+        }
+
+        return new double[]{
+                roti,
+                dal,
+                sabji,
+                rice
+        };
     }
 }
